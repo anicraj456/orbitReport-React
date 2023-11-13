f99be734543dccb4b651206638c8201d30687978
anicraj456 <anicraj123@gmail.com>
2023-11-12 18:00:41 -0600

    assignment completed

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -4,15 +4,14 @@ import './styling.css';
 
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return(
-  <div className="flex-container">
-  {displaySats.map((sat,id) => {
-  return (
-      <button onClick={() => filterByType(sat)} key={id}>{sat} Orbit </button>
-  )
-  })}
-      <button onClick={() => setSat(satData)}>All Orbits</button>
-    </div>
-  );
-
+    <div className="flex-container">
+    {displaySats.map((sat,id) => {
+    return (
+        <button onClick={() => filterByType(sat)} key={id}>{sat} Orbit </button>
+    )
+    })}
+        <button onClick={() => setSat(satData)}>All Orbits</button>
+      </div>
+    );  
 };
 export default Buttons;
